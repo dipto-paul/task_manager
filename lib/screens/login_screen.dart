7880657c3bc10202e714e85e0833e0f7415b344c
Dipto Paul <diptopaul.contact@gmail.com>
2026-08-30 13:30:39 +0600

    added TextField

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/widgets/screen_bg.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -6,10 +7,31 @@ class LoginScreen extends StatefulWidget {
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
-
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ScreenBG(child: Column(
+        children: [
+          SizedBox(height: 180,),
+          Text('Get Started With', style: Theme.of(context).textTheme.titleLarge,),
+          SizedBox(height: 20,),
+          TextFormField(
+            decoration: InputDecoration(
+              hintText: 'Email',
+              prefixIcon: Icon(Icons.email),
+            ),
+          ),
+          TextFormField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Password',
+              prefixIcon: Icon(Icons.password),
+
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../utils/asset_path.dart';
 import '../widgets/screen_bg.dart';
+import '../screens/login_screen.dart';
 
 
 class SplashScreen extends StatefulWidget {
@@ -12,6 +13,18 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    moveToNextScreen();
+  }
+
+  Future moveToNextScreen () async {
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> LoginScreen()));
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
