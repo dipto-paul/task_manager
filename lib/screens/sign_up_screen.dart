6dbb704e0b05068542f69ee4a229bde86c59ade6
Dipto Paul <diptopaul.contact@gmail.com>
2026-08-31 19:58:37 +0600

    prepared for sign_up_screen

---
+++ lib/screens/sign_up_screen.dart
@@ -0,0 +1,15 @@
+
+
+class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  State<SignUpScreen> createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
