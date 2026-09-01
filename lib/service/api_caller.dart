e178fe639e351f240fa6f297ff5a8f2293892a2c
Dipto Paul <diptopaul.contact@gmail.com>
2026-09-01 23:12:26 +0600

    added post method

--- lib/service/api_caller.dart
+++ lib/service/api_caller.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 import 'package:task_manager/models/api_response.dart';
-import '../service/api_caller.dart';
 
 class ApiCaller {
   static Future<ApiResponse>getRequest({required String url}) async{
@@ -17,7 +16,28 @@ class ApiCaller {
     if(response.statusCode == 200){
       return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body),isSuccess: true );
     }else{
-      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body),isSuccess: jsonDecode(response.body) );
+      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body),isSuccess: false );
+    }
+
+  }
+
+  static Future<ApiResponse>postRequest({required String url, Map<String, dynamic>? body}) async{
+    Response response = await post(Uri.parse(url),
+
+        headers: {
+            "content-type": "application/json",
+            "Accept": "application/json",
+        },
+      body: body != null ? jsonEncode(body): null
+    );
+
+    print("url == $url");
+    print("response == ${response.body}");
+
+    if(response.statusCode == 200 || response.statusCode == 201){
+      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body),isSuccess: true );
+    }else{
+      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body),isSuccess: false );
     }
 
   }
