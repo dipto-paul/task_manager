08024084a0fcb946e3388a1a5b505d0ca23b80f2
Dipto Paul <diptopaul.contact@gmail.com>
2026-09-01 12:17:59 +0600

    added taskCard

--- lib/screens/new_task_screen.dart
+++ lib/screens/new_task_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../widgets/task_card.dart';
 
-
 class NewTaskScreen extends StatefulWidget {
   const NewTaskScreen({super.key});
 
@@ -16,23 +15,32 @@ class _NewTaskScreenState extends State<NewTaskScreen> {
     return Scaffold(
       body: Column(
         children: [
+          const SizedBox(height: 20),
+
           SizedBox(
             height: 100,
             child: ListView.separated(
               scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
               itemCount: 4,
-                itemBuilder: (context, index){
-                  return TaskCard(title: 'New', count: 25,);
-                },
-              separatorBuilder: (BuildContext context, int index){
-                return SizedBox(width: 5,);
+
+              itemBuilder: (context, index) {
+                return SizedBox(
+                  width: 87.5,
+                  child: TaskCard(
+                    title: 'New',
+                    count: 25,
+                  ),
+                );
               },
-            )
-          )
+
+              separatorBuilder: (context, index) {
+                return const SizedBox(width: 10);
+              },
+            ),
+          ),
         ],
-      )
+      ),
     );
   }
 }
-
-
--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -3,16 +3,31 @@ import 'package:flutter/material.dart';
 class TaskCard extends StatelessWidget {
   final String title;
   final int count;
+
   const TaskCard({
-    super.key, required this.title, required this.count,
+    super.key,
+    required this.title,
+    required this.count,
   });
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(count.toString(), style: Theme.of(context).textTheme.titleLarge,),
-        Text(title),
-      ],
+    return Card(
+      child: SizedBox(
+        width: double.infinity,
+        height: 80,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              count.toString(),
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Text(title),
+          ],
+        ),
+      ),
     );
   }
 }
