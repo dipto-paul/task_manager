a26f3dbbee53805e9b99918d3cdb7ebf42ac6965
Dipto Paul <diptopaul.contact@gmail.com>
2026-09-02 23:23:24 +0600

    added formKey

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -11,6 +11,14 @@ class SignUpScreen extends StatefulWidget {
 }
 class _SignUpScreenState extends State<SignUpScreen> {
 
+  TextEditingController emailController = TextEditingController();
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController mobileController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   onTapSignIn(){ // Recognizer add korar jonno
     Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen()));
   }
@@ -20,73 +28,76 @@ class _SignUpScreenState extends State<SignUpScreen> {
     return Scaffold(
       body: ScreenBG(child: Padding(
         padding: const EdgeInsets.all(35.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 180,),
-            Text('Join With Us', style: Theme.of(context).textTheme.titleLarge,),
-            SizedBox(height: 20,),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-                prefixIcon: Icon(Icons.email),
+        child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 180,),
+              Text('Join With Us', style: Theme.of(context).textTheme.titleLarge,),
+              SizedBox(height: 20,),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  prefixIcon: Icon(Icons.email),
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
+              SizedBox(height: 10,),
 
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'First Name',
-                prefixIcon: Icon(Icons.drive_file_rename_outline),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'First Name',
+                  prefixIcon: Icon(Icons.drive_file_rename_outline),
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Last Name',
-                prefixIcon: Icon(Icons.drive_file_rename_outline),
+              SizedBox(height: 10,),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Last Name',
+                  prefixIcon: Icon(Icons.drive_file_rename_outline),
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Mobile',
-                prefixIcon: Icon(Icons.call),
+              SizedBox(height: 10,),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Mobile',
+                  prefixIcon: Icon(Icons.call),
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
+              SizedBox(height: 10,),
 
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: 'Password',
-                prefixIcon: Icon(Icons.password),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  prefixIcon: Icon(Icons.password),
+                ),
               ),
-            ),
-            SizedBox(height: 20,),
+              SizedBox(height: 20,),
 
-            FilledButton(onPressed: (){}, child: Icon(Icons.arrow_forward_ios)),
-            SizedBox(height: 50,),
+              FilledButton(onPressed: (){}, child: Icon(Icons.arrow_forward_ios)),
+              SizedBox(height: 50,),
 
-            Center(
-              child: Column(
-                children: [
+              Center(
+                child: Column(
+                  children: [
 
-                  RichText(text: TextSpan(
-                      text:"Already Have an Account?", style: TextStyle(color: Colors.black),
-                      children: [
-                        TextSpan(
-                          text: "Sign In", style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
-                          recognizer: TapGestureRecognizer()..onTap = onTapSignIn,
-                        ),
-                      ]
-                  ),
-                  ),
+                    RichText(text: TextSpan(
+                        text:"Already Have an Account?", style: TextStyle(color: Colors.black),
+                        children: [
+                          TextSpan(
+                            text: "Sign In", style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+                            recognizer: TapGestureRecognizer()..onTap = onTapSignIn,
+                          ),
+                        ]
+                    ),
+                    ),
 
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       )),
     );
