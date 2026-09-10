dd901456fac8bd717f972564b855c3d1005df472
Dipto Paul <diptopaul.contact@gmail.com>
2026-09-10 21:46:17 +0600

    api caller modified

--- lib/service/api_caller.dart
+++ lib/service/api_caller.dart
@@ -1,47 +1,51 @@
 import 'dart:convert';
 
+import '../controller/auth_controller.dart';
+import '../models/api_response.dart';
 import 'package:http/http.dart';
-import 'package:task_manager/controller/auth_controller.dart';
-import 'package:task_manager/models/api_response.dart';
 
 class ApiCaller {
-  static Future<ApiResponse>getRequest({required String url}) async{
+  static Future<ApiResponse>getRequest({required String url}) async {
+
     Response response = await get(Uri.parse(url),
 
         headers: {
-      'token': AuthController.Usertoken ?? '',
+          'token': AuthController.Usertoken ?? '',
         }
     );
 
-    print("url == $url");
-    print("response == ${response.body}");
+    print('URL====$url');
+    print('URL====${response.body}');
 
-    if(response.statusCode == 200){
-      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body),isSuccess: true );
+    if(response.statusCode==200){
+      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body), isSuccess: true);
     }else{
-      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body),isSuccess: false );
+      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body), isSuccess: jsonDecode(response.body));
     }
 
   }
 
-  static Future<ApiResponse>postRequest({required String url, Map<String, dynamic>? body}) async{
+
+  static Future<ApiResponse>postRequest({required String url, Map<String,dynamic>?body}) async {
+
     Response response = await post(Uri.parse(url),
 
         headers: {
-            "content-type": "application/json",
-            "Accept": "application/json",
+          "Content-Type": "application/json",
+          "Accept": "application/json",
           'token': AuthController.Usertoken ?? '',
         },
-      body: body != null ? jsonEncode(body): null
+
+        body: body != null ? jsonEncode(body) : null
     );
 
-    print("url == $url");
-    print("response == ${response.body}");
+    print('URL====$url');
+    print('URL====${response.body}');
 
-    if(response.statusCode == 200 || response.statusCode == 201){
-      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body),isSuccess: true );
+    if(response.statusCode==200 || response.statusCode == 201){
+      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body), isSuccess: true);
     }else{
-      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body),isSuccess: false );
+      return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body), isSuccess: jsonDecode(response.body));
     }
 
   }
--- lib/widgets/task_card.dart
+++ lib/widgets/task_card.dart
@@ -1,38 +1,143 @@
+import 'package:batch_18/module_10/CRUD/utils/urls.dart';
+import 'package:batch_18/task_manager/models/api_response.dart';
+import 'package:batch_18/task_manager/models/task_model.dart';
+import 'package:batch_18/task_manager/service/api_caller.dart';
+import '../utils/urls.dart';
 import 'package:flutter/material.dart';
-import '../models/task_model.dart';
-class TaskCard extends StatelessWidget {
+class TaskCard extends StatefulWidget {
   final TaskModel taskModel;
   final Color cardColor;
   final VoidCallback refreshParent;
-
 
   const TaskCard({
     super.key, required this.taskModel, required this.cardColor, required this.refreshParent,
   });
 
   @override
+  State<TaskCard> createState() => _TaskCardState();
+}
+
+class _TaskCardState extends State<TaskCard> {
+
+  Future<void> deleteTask() async {
+    final ApiResponse response = await ApiCaller.getRequest(url: TMUrls.deleteTaskURL(widget.taskModel.sId.toString()));
+
+    if(response.isSuccess){
+      widget.refreshParent();
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Task Deleted')));
+
+
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('something wrong....!')));
+
+    }
+  }
+
+  Future<void> changeStatusTask(String status) async {
+    final ApiResponse response = await ApiCaller.getRequest(url: TMUrls.updateTaskStatusURL(widget.taskModel.sId.toString(), status));
+
+    setState(() {
+
+    });
+
+    if(response.isSuccess){
+      widget.refreshParent();
+      Navigator.pop(context);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('Task updated')));
+
+
+    }else{
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('something wrong....!')));
+
+    }
+  }
+
+
+  void showChangeStatusDialog(){
+    showDialog(context: context, builder: (context)=>AlertDialog(
+      title: Text('Change Status'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Card(
+            child: ListTile(
+                title: Text('New'),
+                onTap: (){
+                  changeStatusTask('New');
+                },
+                trailing:  widget.taskModel.status == 'New' ? Icon(Icons.check_circle,color: Colors.green,) : null
+
+            ),
+          ),
+          Card(
+            child: ListTile(
+                title: Text('Progress'),
+                onTap: (){
+                  changeStatusTask('Progress');
+                },
+
+                trailing:  widget.taskModel.status == 'Progress' ? Icon(Icons.check_circle,color: Colors.green,) : null
+
+            ),
+          ),
+          Card(
+            child: ListTile(
+                title: Text('Completed'),
+                onTap: (){
+                  changeStatusTask('Completed');
+                },
+
+                trailing:  widget.taskModel.status == 'Completed' ? Icon(Icons.check_circle,color: Colors.green,) : null
+
+            ),
+          ),
+          Card(
+            child: ListTile(
+                title: Text('Cancelled'),
+                onTap: (){
+                  changeStatusTask('Cancelled');
+                },
+
+                trailing:  widget.taskModel.status == 'Cancelled' ? Icon(Icons.check_circle,color: Colors.green,) : null
+
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+
+
+
+  @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        title: Text(taskModel.title.toString(),style: Theme.of(context).textTheme.titleLarge!.copyWith(
-          fontSize: 18,
+        title: Text(widget.taskModel.title.toString(),style: Theme.of(context).textTheme.titleLarge!.copyWith(
+            fontSize: 18
         ),),
         subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(taskModel.description.toString()),
+            Text(widget.taskModel.description.toString()),
             SizedBox(height: 5,),
-            Text(taskModel.createdDate.toString()),
+            Text('Date:${widget.taskModel.createdDate}'),
 
             Row(
               children: [
-                Chip(label: Text(taskModel.status.toString(),style: TextStyle(color: Colors.white),),
-                  backgroundColor: Colors.blue,
+                Chip(
+                  label: Text(widget.taskModel.status.toString(),style: TextStyle(color: Colors.white),),
+                  backgroundColor: widget.cardColor,
                 ),
 
                 Spacer(),
-                IconButton(onPressed: (){}, icon: Icon(Icons.edit,color: Colors.orange,)),
-                IconButton(onPressed: (){}, icon: Icon(Icons.delete,color: Colors.red,)),
+
+                IconButton(onPressed: (){
+                  showChangeStatusDialog();
+                },icon: Icon(Icons.edit_note,color: Colors.orange,),),
+                IconButton(onPressed: (){
+                  deleteTask();
+                },icon: Icon(Icons.delete,color: Colors.red,),),
               ],
             )
           ],
