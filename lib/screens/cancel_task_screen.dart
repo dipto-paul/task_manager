8b567dfeb3c834ffad3193a79eaef25e0727582c
Dipto Paul <diptopaul.contact@gmail.com>
2026-09-11 12:23:58 +0600

    indecator added in cancel task screen

--- lib/screens/cancel_task_screen.dart
+++ lib/screens/cancel_task_screen.dart
@@ -5,6 +5,7 @@ import '../models/task_model.dart';
 import '../service/api_caller.dart';
 import '../utils/urls.dart';
 import '../widgets/task_card.dart';
+
 class CancelTaskScreen extends StatefulWidget {
   const CancelTaskScreen({super.key});
 
@@ -14,50 +15,70 @@ class CancelTaskScreen extends StatefulWidget {
 
 class _CancelTaskScreenState extends State<CancelTaskScreen> {
 
+  bool isLoading = true;
+
+  List<TaskModel> taskList = [];
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     getTask('Cancelled');
   }
 
-  List<TaskModel> taskList = [];
+  Future<void> getTask(String status) async {
 
-  Future <void> getTask(String status) async {
-    final ApiResponse response= await ApiCaller.getRequest(url: TMUrls.taskListByStatusURL(status));
+    final ApiResponse response = await ApiCaller.getRequest(
+      url: TMUrls.taskListByStatusURL(status),
+    );
 
-    List<TaskModel> tList= [];
+    List<TaskModel> tList = [];
 
-    if(response.isSuccess){
-      for(Map<String,dynamic>jsonData in response.responseData['data']){
+    if (response.isSuccess) {
+
+      for (Map<String, dynamic> jsonData
+      in response.responseData['data']) {
         tList.add(TaskModel.fromJson(jsonData));
       }
-    }else{
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(response.responseData['data'])));
 
+    } else {
+
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              response.responseData['data'].toString(),
+            ),
+          ),
+        );
+      }
     }
 
-    if(mounted){
+    if (mounted) {
       setState(() {
         taskList = tList;
+        isLoading = false;
       });
     }
-
-
-
   }
-
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-
-          itemCount: taskList.length,
-          itemBuilder: (context,index){
-            return TaskCard(taskModel: taskList[index], cardColor: Colors.red, refreshParent: () {  },);
-          }
+      body: isLoading ? const Center(
+        child: CircularProgressIndicator(
+          color: Colors.green,
+          backgroundColor: Colors.grey,
+          strokeWidth: 7,
+        ),
+      ) : ListView.builder(
+        itemCount: taskList.length,
+        itemBuilder: (context, index) {
+          return TaskCard(
+            taskModel: taskList[index],
+            cardColor: Colors.red,
+            refreshParent: () {},
+          );
+        },
       ),
     );
   }
